40ebc7fc6dcd76266a6c57199581dd87dbd24bf4
TomiukQT <TomiukQT@gmail.com>
2019-09-08 15:27:59 +0200

    Basic Funcionality

---
+++ BohemiaTest/Assets/Scripts/PlantController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlantState {Base, Burning, Burnt};
+
+public class PlantController : MonoBehaviour
+{
+
+
+    public Material m_BurningMaterial;
+    public Material m_BurntMaterial;
+    public PlantState m_State = PlantState.Base;
+
+    private bool m_IsBurning = false;
+    private int m_CurrentBurning = 0;
+    public int m_BurningDuration = 10;
+
+    private float m_BurningRadius = 10f;
+
+    private void Awake()
+    {
+        TimeTickSystem.OnTick += TimeTickSystem_OnTick;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void TimeTickSystem_OnTick(object sender, TimeTickSystem.OnTickEventArgs e)
+    {
+        //Debug.Log("TICK: " + e.ticks);
+        UpdateState();
+        SpreadFire();
+    }
+
+    public void StartBurning()
+    {
+        if (m_State != PlantState.Base)
+            return;
+        m_IsBurning = true;
+        gameObject.GetComponent<Renderer>().material = m_BurningMaterial;
+        m_State = PlantState.Burning;
+    }
+
+    void UpdateState()
+    {
+        if(m_IsBurning)
+        {
+            m_CurrentBurning++;
+            if(m_CurrentBurning >= m_BurningDuration)
+            {
+                m_IsBurning = false;
+                m_State = PlantState.Burnt;
+                gameObject.GetComponent<Renderer>().material = m_BurntMaterial;
+            }
+        }
+    }
+
+    void SpreadFire()
+    {
+        if (m_State != PlantState.Burning)
+            return;
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_BurningRadius, LayerMask.GetMask("Plants"));
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            colliders[i].gameObject.GetComponent<PlantController>().StartBurning();
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, m_BurningRadius);
+    }
+
+    private void OnMouseDown()
+    {
+        StartBurning();
+    }
+}
---
+++ BohemiaTest/Assets/Scripts/PlantGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantGenerator : MonoBehaviour
+{
+    public int m_AmountToGenerate = 1000;
+
+    public GameObject m_Plant;
+
+    private Terrain m_Terrain;
+
+    private GameObject m_PlantsParent;
+
+    private void Awake()
+    {
+        m_Terrain = GameObject.Find("Terrain").GetComponent<Terrain>();
+    }
+
+    private void Start()
+    {
+        Generate();
+    }
+
+    void Generate()
+    {
+        if (m_PlantsParent != null)
+        {
+            Destroy(m_PlantsParent);
+        }
+        m_PlantsParent = new GameObject("PlantsParent");
+        for (int i = 0; i < m_AmountToGenerate; i++)
+        {
+            float x = Random.Range(0f, m_Terrain.terrainData.size.x);
+            float z = Random.Range(0f, m_Terrain.terrainData.size.z);
+            float y = m_Terrain.SampleHeight(new Vector3(x, 0, z));
+            y += m_Plant.transform.localScale.y/2;
+            GameObject spawned = Instantiate(m_Plant, new Vector3(x, y, z), Quaternion.identity);
+            spawned.transform.SetParent(m_PlantsParent.transform);
+
+        }
+
+    }
+
+
+}
