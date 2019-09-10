b926dc22c5d0d30f28a7ad9ef742000e8a4d3adf
TomiukQT <TomiukQT@gmail.com>
2019-09-10 20:46:00 +0200

    Wind

--- BohemiaTest/Assets/Scripts/PlantController.cs
+++ BohemiaTest/Assets/Scripts/PlantController.cs
@@ -7,20 +7,28 @@ public enum PlantState {Base, Burning, Burnt};
 public class PlantController : MonoBehaviour
 {
 
-
+    public Material m_StartedBurningMaterial;
     public Material m_BurningMaterial;
     public Material m_BurntMaterial;
     public PlantState m_State = PlantState.Base;
+
+    private float m_CurrentFire = 0f;
+    private float m_MaxFire = 5f;
 
     private bool m_IsBurning = false;
     private int m_CurrentBurning = 0;
     public int m_BurningDuration = 10;
 
-    private float m_BurningRadius = 10f;
+    private float m_BurningRadius = 25f;
+    private Vector3 m_BurningCenter;
 
+    private Wind m_WindManager;
     private void Awake()
     {
         TimeTickSystem.OnTick += TimeTickSystem_OnTick;
+        m_BurningCenter = transform.position;
+
+        m_WindManager = GameObject.Find("GameManager").GetComponent<Wind>();
     }
 
     private void Start()
@@ -32,12 +40,17 @@ public class PlantController : MonoBehaviour
     {
         //Debug.Log("TICK: " + e.ticks);
         UpdateState();
+        WindCorrection();
         SpreadFire();
     }
 
-    public void StartBurning()
+    public void StartBurning(float amount = 1f)
     {
         if (m_State != PlantState.Base)
+            return;
+        m_CurrentFire+= amount;
+        gameObject.GetComponent<Renderer>().material = m_StartedBurningMaterial;
+        if (m_CurrentFire < m_MaxFire)
             return;
         m_IsBurning = true;
         gameObject.GetComponent<Renderer>().material = m_BurningMaterial;
@@ -58,11 +71,19 @@ public class PlantController : MonoBehaviour
         }
     }
 
+    void WindCorrection()
+    {
+        Vector3 correction = new Vector3(Mathf.Sin((m_WindManager.m_Rotation+90) * Mathf.Deg2Rad), 
+                                         0,
+                                         Mathf.Cos((m_WindManager.m_Rotation+90) * Mathf.Deg2Rad));
+        m_BurningCenter = transform.position + (correction * m_WindManager.m_Strength);
+    }
+
     void SpreadFire()
     {
         if (m_State != PlantState.Burning)
             return;
-        Collider[] colliders = Physics.OverlapSphere(transform.position, m_BurningRadius, LayerMask.GetMask("Plants"));
+        Collider[] colliders = Physics.OverlapSphere(m_BurningCenter, m_BurningRadius, LayerMask.GetMask("Plants"));
         for (int i = 0; i < colliders.Length; i++)
         {
             colliders[i].gameObject.GetComponent<PlantController>().StartBurning();
@@ -72,11 +93,11 @@ public class PlantController : MonoBehaviour
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, m_BurningRadius);
+        Gizmos.DrawWireSphere(m_BurningCenter, m_BurningRadius);
     }
 
     private void OnMouseDown()
     {
-        StartBurning();
+        StartBurning(m_MaxFire);
     }
 }
---
+++ BohemiaTest/Assets/Scripts/Wind.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    public float m_Rotation;
+    public float m_Strength;
+
+    public Transform m_Arrow;
+
+    private void Update()
+    {
+        UpdateArrow();
+    }
+
+    void UpdateArrow()
+    {
+        m_Arrow.eulerAngles = new Vector3(0, m_Rotation, 0);
+    }
+
+
+}
